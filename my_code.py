b87f0094c322fd4b31e4e6577a4c1d1525ba2fc5
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-08-09 14:04:33 -0700

    Create my-age assignment template

--- my_code.py
+++ my_code.py
@@ -1,13 +1,34 @@
-def inc(x):
-    # User code goes here
-    return x + 1
+# Complete the function below calculate your age after different numbers of years.
+
+def age_now(x):
+    # My age now
+    print(f"I am currently { } years old.\n")
+
+def age_1(y):
+    # My age next year
+    print(f"Next year I'll be { } years old.\n")
+
+def age_10(z):
+    # My age in 10 years
+    print(f"In 10 years, I'll be { }!\n")
+
+def age_50(q):
+    # My age in 50 years!
+    print(f"In 50 years, I'll be { }! Wow!\n")
+
+
 
 
 if __name__ == '__main__':
-    # Test your code with this first
-    # Change the argument to try different values
-    print(inc(6))
 
-    # After you are satisfied with your results, use input() to prompt the user for a value:
-    #v = input("Value to increment: ")
-    #print(inc(int(v)))
+    age = 15    # change this number to your current age
+
+    '''
+    after you have this working by defining "age" above, you can comment that line out and add a new line
+    # below it to get the user's age as an input.
+    '''
+
+    age_now(age)   # run function age_now with argument age
+    age_1(age)    # run function age_1 with argument age
+    age_10(age)    # run function age_10 with argument age
+    age_50(age)    # run function age_50 with argument age
